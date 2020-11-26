20eefa51a452eeeb03d5573b14ea7f63d92f7792
Mikhail Kozitskii <kekouke@yandex.ru>
2020-11-27 00:41:08 +1000

    Add a custom font

--- Notes/Notes/AssemblyInfo.cs
+++ Notes/Notes/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Roboto-Regular.ttf", Alias ="Roboto")]
+[assembly: ExportFont("Lobster-Regular.ttf", Alias = "Lobster")]
